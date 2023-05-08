ab63ad141db246ed06ec2230ec146de2f51706e9
Rishat Agzamov <rishat.se@gmail.com>
2023-05-08 17:28:24 -0400

    feat: basic SSG API fetching

---
+++ src/constants/api.ts
@@ -0,0 +1,1 @@
+export const apiURL = 'https://rickandmortyapi.com/graphql';
--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -1,8 +1,38 @@
 import Layout from '@/components/Layout/Layout';
 import styles from '@/styles/main.module.scss';
+import { GraphQLClient, gql } from 'graphql-request';
 import Head from 'next/head';
+import { apiURL } from '../constants/api';
 
-export default function Main() {
+export const getStaticProps = async () => {
+  const query = gql`
+    query {
+      __type(name: "Character") {
+        fields {
+          name
+        }
+      }
+    }
+  `;
+  const client = new GraphQLClient(apiURL, { headers: {} });
+  const data = await client.request(query);
+  // console.log(JSON.stringify(data));
+  const curTime = new Date().toLocaleTimeString();
+  console.log(curTime);
+  return {
+    props: {
+      data,
+      curTime,
+    },
+  };
+};
+
+type MainProps = {
+  data: object;
+  curTime: Date;
+};
+
+export default function Main({ data, curTime }: MainProps) {
   return (
     <>
       <Head>
@@ -12,7 +42,12 @@ export default function Main() {
         <div className={styles.container}>
           <div>
             <label htmlFor="request">
-              <textarea name="request" rows={30} cols={50} defaultValue="Some code"></textarea>
+              <textarea
+                name="request"
+                rows={30}
+                cols={50}
+                defaultValue={`${curTime} - ${JSON.stringify(data)}`}
+              ></textarea>
             </label>
           </div>
           <button className={styles.button} type="button">
