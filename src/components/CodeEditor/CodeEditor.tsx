da12ad96fdf832266c1f4825b801ec417d9c38b0
KohnoA <san.kohno@yandex.ru>
2023-05-11 20:48:42 +0300

    feat: moved the code editor to a separate component

---
+++ src/components/CodeEditor/CodeEditor.tsx
@@ -0,0 +1,85 @@
+import styles from './CodeEditor.module.scss';
+import React, { useState, useCallback } from 'react';
+import CodeMirror from '@uiw/react-codemirror';
+import { ViewUpdate } from '@codemirror/view';
+import { CODEMIRROR_THEME, CODEMIRROR_EXTENSIONS } from '@/constants/codeMirrorSettings';
+import PlayIcon from '@/components/icons/PlayIcon';
+import Image from 'next/image';
+import ArrowImg from '../../../public/icons/up-arrow.svg';
+import Link from 'next/link';
+
+export default function CodeEditor() {
+  const [reqValue, setReqValue] = useState<string>('');
+  const [resValue, setResValue] = useState<string>('');
+  const [showTools, setShowTools] = useState<boolean>(false);
+
+  const onChange = useCallback((value: string, viewUpdate: ViewUpdate) => setReqValue(value), []);
+
+  const requestHandler = () => {
+    console.log(reqValue);
+    setResValue(reqValue);
+  };
+
+  return (
+    <section className={`${styles.editor} ${styles.editor__wrapper}`}>
+      <div className={styles.editor__input}>
+        <div className={styles.editor__wrapper}>
+          <CodeMirror
+            className={styles.editor__mirror}
+            theme={CODEMIRROR_THEME}
+            height="100%"
+            value={reqValue}
+            extensions={CODEMIRROR_EXTENSIONS}
+            onChange={onChange}
+          />
+
+          <button className={styles.editor__reqButton} onClick={requestHandler}>
+            <PlayIcon />
+          </button>
+        </div>
+
+        <div className={styles.tools}>
+          <div>
+            <button className={styles.tools__button}>Variables</button>
+            <button className={styles.tools__button}>Headers</button>
+            <button
+              className={`${styles.tools__button} ${styles.tools__toggler}`}
+              onClick={() => setShowTools((prev) => !prev)}
+            >
+              <Image
+                {...(showTools ? { className: styles.tools__toggler_hidden } : {})}
+                src={ArrowImg}
+                width={12}
+                height={12}
+                alt="Show or hidden tools editor"
+              />
+            </button>
+          </div>
+
+          {showTools && (
+            <CodeMirror
+              className={styles.editor__mirror}
+              theme={CODEMIRROR_THEME}
+              height="100px"
+              value=""
+              extensions={CODEMIRROR_EXTENSIONS}
+              onChange={onChange}
+            />
+          )}
+        </div>
+      </div>
+
+      <div className={styles.editor__output}>
+        <CodeMirror
+          className={styles.editor__mirror}
+          theme={CODEMIRROR_THEME}
+          height="100%"
+          value={resValue}
+          extensions={CODEMIRROR_EXTENSIONS}
+          editable={false}
+          basicSetup={{ lineNumbers: false }}
+        />
+      </div>
+    </section>
+  );
+}
--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -1,25 +1,10 @@
 import Layout from '@/components/Layout/Layout';
 import styles from '@/styles/main.module.scss';
 import Head from 'next/head';
-import { useState, useCallback } from 'react';
 import ControlsPanel from '@/components/ControlsPanel/ControlsPanel';
-import CodeMirror from '@uiw/react-codemirror';
-import { ViewUpdate } from '@codemirror/view';
-import { CODEMIRROR_THEME, CODEMIRROR_EXTENSIONS } from '@/constants/codeMirrorSettings';
-import PlayIcon from '@/components/icons/PlayIcon';
+import CodeEditor from '@/components/CodeEditor/CodeEditor';
 
 export default function Main() {
-  const [reqValue, setReqValue] = useState<string>('');
-  const [resValue, setResValue] = useState<string>('');
-  const [showTools, setShowTools] = useState<boolean>(false);
-
-  const onChange = useCallback((value: string, viewUpdate: ViewUpdate) => setReqValue(value), []);
-
-  const requestHandler = () => {
-    console.log(reqValue);
-    setResValue(reqValue);
-  };
-
   return (
     <>
       <Head>
@@ -30,60 +15,7 @@ export default function Main() {
         <main className={styles.container}>
           <ControlsPanel />
 
-          <section className={`${styles.editor} ${styles.editor__wrapper}`}>
-            <div className={styles.editor__input}>
-              <div className={styles.editor__wrapper}>
-                <CodeMirror
-                  className={styles.editor__mirror}
-                  theme={CODEMIRROR_THEME}
-                  height="100%"
-                  value={reqValue}
-                  extensions={CODEMIRROR_EXTENSIONS}
-                  onChange={onChange}
-                />
-
-                <button className={styles.editor__reqButton} onClick={requestHandler}>
-                  <PlayIcon />
-                </button>
-              </div>
-
-              <div className={styles.tools}>
-                <div>
-                  <button className={styles.tools__button}>Variables</button>
-                  <button className={styles.tools__button}>Headers</button>
-                  <button
-                    className={`${styles.tools__button} ${styles.tools__button_toggle}`}
-                    onClick={() => setShowTools((prev) => !prev)}
-                  >
-                    O
-                  </button>
-                </div>
-
-                {showTools && (
-                  <CodeMirror
-                    className={styles.editor__mirror}
-                    theme={CODEMIRROR_THEME}
-                    height="100px"
-                    value=""
-                    extensions={CODEMIRROR_EXTENSIONS}
-                    onChange={onChange}
-                  />
-                )}
-              </div>
-            </div>
-
-            <div className={styles.editor__output}>
-              <CodeMirror
-                className={styles.editor__mirror}
-                theme={CODEMIRROR_THEME}
-                height="100%"
-                value={resValue}
-                extensions={CODEMIRROR_EXTENSIONS}
-                editable={false}
-                basicSetup={{ lineNumbers: false }}
-              />
-            </div>
-          </section>
+          <CodeEditor />
         </main>
       </Layout>
     </>
