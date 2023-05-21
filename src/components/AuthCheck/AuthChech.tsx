d1397ea94caaec367d02473524038ba62ebcac4e
Aliaksandr Konik <cibulyadev@gmail.com>
2023-05-21 15:53:56 +0200

    fix:fix route bug

--- src/components/AuthCheck/AuthChech.tsx
+++ src/components/AuthCheck/AuthChech.tsx
@@ -11,14 +11,16 @@ export default function AuthCheck({ children }: { children: React.ReactNode }) {
   const { signIn, singOut } = authSlice.actions;
   const dispatch = useAppDispatch();
   const router = useRouter();
-  const { pathname } = useRouter();
+  const { pathname, asPath, route, events } = useRouter();
 
+  //experimental feature
   const tokenTest = async () => {
     const expTime = await getAuth().currentUser?.getIdToken();
     if (expTime) {
       const decoded = jwt.decode(expTime, { complete: true })?.payload as JwtPayload;
-      const timeStamp = Math.floor(Date.now() / 1000);
+
       setInterval(() => {
+        const timeStamp = Math.floor(Date.now() / 1000);
         if (decoded.exp)
           if (timeStamp >= decoded.exp) {
             dispatch(singOut());
@@ -36,7 +38,11 @@ export default function AuthCheck({ children }: { children: React.ReactNode }) {
     getAuth().onAuthStateChanged((user) => {
       if (user) {
         dispatch(signIn({ isAuth: true, email: user.email }));
+        pathname.replace(pathname, '/main');
         router.push(pathname !== '' ? '/main' : pathname);
+
+        console.log(pathname, 'pathname');
+        console.log(route, 'route');
         setIsLoading(false);
       } else {
         setIsLoading(false);
