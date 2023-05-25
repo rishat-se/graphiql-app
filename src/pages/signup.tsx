44c5b12b9e9c22d0036567257b1a2a423c6a7d72
KohnoA <san.kohno@yandex.ru>
2023-05-25 22:53:27 +0300

    feat: implemented the translation of the sign up page

--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -2,17 +2,29 @@ import Form from '@/components/Form/Form';
 import Layout from '@/components/Layout/Layout';
 import Loading from '@/components/Loading/Loading';
 import { useAppSelector } from '@/hooks/redux';
+import { useTranslation } from 'next-i18next';
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import Head from 'next/head';
 
 export default function Auth() {
   const { isAuth } = useAppSelector((state) => state.authReducer);
   const notAuthrized = isAuth === false;
+  const { t } = useTranslation('signUp');
+
   return (
     <>
       <Head>
-        <title>GraphiQL : Sign up</title>
+        <title>GraphiQL : {t('page-title')}</title>
       </Head>
       <Layout>{notAuthrized ? <Form /> : <Loading />}</Layout>
     </>
   );
+}
+
+export async function getStaticProps({ locale }: { locale: string }) {
+  return {
+    props: {
+      ...(await serverSideTranslations(locale, ['common', 'signUp', 'form'])),
+    },
+  };
 }
