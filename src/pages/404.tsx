becd8436ce901e6236d7d5e196610d5805e862fd
KohnoA <san.kohno@yandex.ru>
2023-05-25 15:56:06 +0300

    feat: implemented the translation of the 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,26 +1,38 @@
 import Layout from '@/components/Layout/Layout';
 import styles from '@/styles/notfound.module.scss';
+import { useTranslation } from 'next-i18next';
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import Head from 'next/head';
 import Link from 'next/link';
 
 export default function NotFound() {
+  const { t } = useTranslation('404');
+
   return (
     <>
       <Head>
-        <title>GraphiQL : Not Found</title>
+        <title>GraphiQL : {t('page-title')}</title>
       </Head>
       <Layout>
         <div className={styles.container}>
           <div className={styles.content}>
-            <h1 className={styles.title}>Something went wrong</h1>
-            <p className={styles.error}>Error 404</p>
-            <p className={styles.error__desc}>Page don&apos;t exists,Morty!</p>
+            <h1 className={styles.title}>{t('title')}</h1>
+            <p className={styles.error}>{t('error')}</p>
+            <p className={styles.error__desc}>{t('error-desc')}</p>
             <Link className={styles.link} href="/">
-              Return to Home Page
+              {t('link-to-init')}
             </Link>
           </div>
         </div>
       </Layout>
     </>
   );
+}
+
+export async function getStaticProps({ locale }: { locale: string }) {
+  return {
+    props: {
+      ...(await serverSideTranslations(locale, ['common', '404'])),
+    },
+  };
 }
