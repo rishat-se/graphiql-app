9656572c2b7afc4daa3d08392b365cba4b4415b5
Aliaksandr Konik <cibulyadev@gmail.com>
2023-05-27 01:02:53 +0200

    fix:fix loading bug fix fonts issue

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -2,5 +2,5 @@ module.exports = {
   i18n: {
     locales: ['en', 'by'],
     defaultLocale: 'en',
-  }
-}
+  },
+};
--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,8 +1,6 @@
 import Footer from '@/components/Footer/Footer';
 import Header from '@/components/Header/Header';
-import { useRouter } from 'next/router';
 export default function Layout({ children }: { children: React.ReactNode }) {
-  const { locale } = useRouter();
   return (
     <>
       <Header />
