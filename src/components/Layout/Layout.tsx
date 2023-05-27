9c3e0286caf8ea9bd532cb8a8ef92cb392591e41
Rishat Agzamov <rishat.se@gmail.com>
2023-05-27 09:43:11 -0400

    feat: add style to Layout

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,11 +1,12 @@
+import styles from './layout.module.scss';
 import Footer from '@/components/Footer/Footer';
 import Header from '@/components/Header/Header';
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <>
+    <div className={styles.layout}>
       <Header />
       {children}
       <Footer />
-    </>
+    </div>
   );
 }
